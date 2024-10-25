bf295dcbb33364c8dab2fd04dfa36cb030f35862
Ronald Benitez <105622544+RonaldBenktez@users.noreply.github.com>
2024-10-24 20:07:09 -0600

    Add files via upload

---
+++ Practica9/p9.kt
@@ -0,0 +1,37 @@
+fun fibonacci(n: Int): Int {
+    return if (n <= 1) {
+        n
+    } else {
+        fibonacci(n-1) + fibonacci(n-2)
+    }
+}
+
+fun sumaRecursiva(n: Int): Int {
+    return if (n <= 1) {
+        n
+    } else {
+        n + sumaRecursiva(n-1)
+    }
+}
+
+fun sumaLista(lista: List<Int>): Int {
+    return if (lista.isEmpty()) {
+        0
+    } else {
+        lista[0] + sumaLista(lista.drop(1))
+    }
+}
+
+fun main(args: Array<String>) {
+    // Ejemplo de Fibonacci
+    val numero = 15
+    val numeroLista = listOf(1, 2, 3, 4)
+    println("Ejemplo 1")
+    println("Fibonacci de $numero es: ${fibonacci(numero)}")
+    
+    println("\nEjemplo 2")
+    println("La suma de 1 a $numero es: ${sumaRecursiva(numero)}")
+    
+    println("\nEjemplo 3")
+    println("La suma de la lista es: ${sumaLista(numeroLista)}")
+}
